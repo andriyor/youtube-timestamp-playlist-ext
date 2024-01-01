02c5b5825b2929481745e73316efe1dfbf13447c
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-01 18:07:28 +0200

    change webpack config create src dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    background_scripts: "./background_scripts/background.js",
-    content_scripts: "./content_scripts/content_scripts.js",
-    popup: "./popup/popup.js"
+    background_scripts: "./src/background.js",
+    content_scripts: "./src/content_scripts.js",
+    popup: "./src/popup.js"
   },
   mode: 'none',
   plugins: [
